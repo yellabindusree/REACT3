f4ff19acebf9494a77f3e66508db06ae030dc25f
yellabindusree <yellabindusree@gmail.com>
2023-10-14 04:06:18 +0530

    commit

---
+++ task3/src/App.js
@@ -0,0 +1,14 @@
+import './App.css';
+
+// import Button from 'react-bootstrap/Button';
+
+
+import { Container } from './components/Container';
+
+function App() {
+  return (
+    <Container/>
+  );
+}
+
+export default App;
